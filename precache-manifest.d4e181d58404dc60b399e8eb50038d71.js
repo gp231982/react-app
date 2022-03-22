2b20d2198cf75c83b6086e2c78ec308926a3f226
Grzegorz Piejko <gp23.1982@gmail.com>
2022-03-22 06:59:31 +0100

    Updates

---
+++ precache-manifest.d4e181d58404dc60b399e8eb50038d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa0cc9f943408e4689b1",
+    "url": "/react-app/static/css/main.a2072f93.chunk.css"
+  },
+  {
+    "revision": "fa0cc9f943408e4689b1",
+    "url": "/react-app/static/js/main.3cd2a7fc.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "4c59afacec54cb918c28",
+    "url": "/react-app/static/js/2.da5fdfa2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9d608f11588f212c4c50e3d9b70a0d3",
+    "url": "/react-app/index.html"
+  }
+];
